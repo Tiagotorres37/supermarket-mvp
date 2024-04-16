6c29a9d650f46c65463f6cb036862f5bf45ee95a
Santiago Torres Arenas <santiagotorresarenas2018@gmail.com>
2024-04-16 18:46:05 -0500

    Pay Mode View

--- obj/Debug/net7.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8d125460cbcd7de05f51f7be4c78dfa0793597b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
