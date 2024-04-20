2911eee4aeb967713d6c598865fae6baa53904ca
Tiagotorres37 <santiagotorresarenas2018@gmail.com>
2024-04-19 23:48:09 -0500

    Provider Model

--- obj/Debug/net7.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+117bae7f9652004d2ffbcf70b4a4f45fe0ebdd52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ce6b4082f37bdac88707511ad568519c4e4fc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
