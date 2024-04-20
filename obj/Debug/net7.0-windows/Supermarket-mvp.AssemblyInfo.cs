2416791c1b0d4c6952d4d694bc316c2623a40438
Tiagotorres37 <santiagotorresarenas2018@gmail.com>
2024-04-20 11:41:29 -0500

    Categorie Model

--- obj/Debug/net7.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+210589b3d3b307ed81c72233902affa6651f98fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+871b1111e5bee98de52d312cfbacfbde2a879a04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
