117bae7f9652004d2ffbcf70b4a4f45fe0ebdd52
Tiagotorres37 <santiagotorresarenas2018@gmail.com>
2024-04-19 23:40:19 -0500

    Product Repository

--- obj/Debug/net7.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5d651b2ce950ab7c88556e95189eb4e3d0a9a67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21b512b8a420b9ed4f948a9a8fcdedb2d274d79c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
